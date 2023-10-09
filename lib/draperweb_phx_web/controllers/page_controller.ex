249617ae298438511c905b83e4c532347c1ca2e7
Natasha Draper <natasha.draper@entelect.co.za>
2023-10-09 08:34:11 +0000

    feat: app layout complete

--- lib/draperweb_phx_web/controllers/page_controller.ex
+++ lib/draperweb_phx_web/controllers/page_controller.ex
@@ -2,8 +2,9 @@ defmodule DraperwebPhxWeb.PageController do
   use DraperwebPhxWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home)
+    conn
+    |> assign(:page_title, "Home")
+    |> assign(:active_route, :home)
+    |> render(:home)
   end
 end
